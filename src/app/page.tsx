2a83c3ae0cd09f02f337b7af13eee444964ce287
Tonny Bett <betttonny26@gmail.com>
2025-06-01 12:44:47 +0000

    remove our services in the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,25 @@
 
 import { HeroSection } from "@/components/sections/HeroSection";
 import { ServiceHighlightsSection } from "@/components/sections/ServiceHighlightsSection";
-import { ProductsSection } from "@/components/sections/ProductsSection";
+// import { ProductsSection } from "@/components/sections/ProductsSection"; // Removed
 import { TestimonialSection } from "@/components/sections/TestimonialSection";
 import { CallToActionStripSection } from "@/components/sections/CallToActionStripSection";
 import { ContactSection } from "@/components/sections/ContactSection";
-import { getHomepageSettings } from "@/data/homepageSettings"; // Import the settings function
+import { getHomepageSettings } from "@/data/homepageSettings";
+import { unstable_noStore as noStore } from 'next/cache';
 
 export default function HomePage() {
-  const homepageSettings = getHomepageSettings();
+  noStore(); // Ensures fresh data for homepage settings
+  const homepageSettings = getHomepageSettings(); 
 
   return (
     <>
-      <HeroSection 
+      <HeroSection
         imageUrl={homepageSettings.heroImageUrl}
         imageHint={homepageSettings.heroImageHint}
       />
       <ServiceHighlightsSection id="about" />
-      <ProductsSection id="products" />
+      {/* <ProductsSection id="products" /> */} {/* This section has been removed */}
       <TestimonialSection />
       <CallToActionStripSection />
       <ContactSection id="contact" />
