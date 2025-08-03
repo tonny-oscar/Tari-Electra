914bbc7ed42a02b7680aa17bea830838a7ef7039
tonny-oscar <betttonny26@gmail.com>
2025-08-03 19:06:41 +0300

    changes

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,5 +1,4 @@
-import { initializeApp, cert, getApps } from 'firebase-admin/app';
-import { getFirestore } from 'firebase-admin/firestore';
+import admin from 'firebase-admin';
 
 
 const serviceAccount = {
@@ -15,9 +14,13 @@ const serviceAccount = {
   client_x509_cert_url: `https://www.googleapis.com/robot/v1/metadata/x509/${process.env.FIREBASE_CLIENT_EMAIL}`
 };
 
-const app = getApps().length === 0 ? initializeApp({
-  credential: cert(serviceAccount as any),
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-}) : getApps()[0];
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount as any),
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  });
+}
 
-export const adminDb = getFirestore(app);
+export const adminDb = admin.firestore();
+export const auth = admin.auth();
+export { admin };
